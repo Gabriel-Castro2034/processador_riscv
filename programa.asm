c8aff8bcff3791d9c7c8914a205b8c015972fa30
Gabriel2034 <127605763+Gabriel-Castro2034@users.noreply.github.com>
2025-08-13 15:43:49 -0300

    Update programa.asm

--- programa.asm
+++ programa.asm
@@ -1,22 +1,26 @@
-addi x1, x0, 10
-addi x2, x0, 10
-addi x3, x0, 20
-sw x3, 0(x0)
-beq x1, x2, L1_pula_erro
+addi x5, x0, 50
+addi x6, x0, 10
+sw x5, 0(x0)
+lw x7, 0(x0)
+sub x10, x7, x6
+addi x11, x0, 25
+xor x12, x10, x11
+addi x13, x0, 2
+srl x14, x12, x13
+addi x15, x0, 12
+beq x14, x15, PASSA
 
-addi x4, x0, 999
+addi x1, x0, 999
 
-L1_pula_erro:
-lw x5, 0(x0)
-sub x6, x5, x1
+PASSA:
+addi x16, x0, 99
+beq x14, x16, FALHA
 
-beq x5, x6, L2_pula_acerto
-
-sw x6, 4(x0)
+sw x14, 8(x0)
 beq x0, x0, FIM
 
-L2_pula_acerto:
-sw x4, 4(x0)
+FALHA:
+sw x1, 8(x0)
 
 FIM:
 beq x0, x0, FIM
